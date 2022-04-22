f072aac0d95bbcab3a94f2574566d32c62e60de0
csuchorski <cezary.suchorski@wp.pl>
2022-04-22 22:08:56 +0200

    Created ChatHub with methods for joining, leaving and finding available rooms

---
+++ ChatAppSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Dictionary<string, bool> groupNames = new Dictionary<string, bool>();
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("ReceiveMessage",$"{Context.ConnectionId} joined the room.");
+        }
+        public async Task LeaveRoom(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} left the room.");
+        }
+        
+        public string FindRoom()
+        {
+            foreach(var room in groupNames)
+            {
+                if (room.Value) return room.Key;
+            }
+            return "roomNull";        
+        }
+
+    }
+}
