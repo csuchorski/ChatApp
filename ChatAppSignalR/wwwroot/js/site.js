fd94a6178f72619ed292067e20bd755aff1e7e99
csuchorski <cezary.suchorski@wp.pl>
2022-04-23 15:47:52 +0200

    Started writing client connection code (websocket error during connection)

--- ChatAppSignalR/wwwroot/js/site.js
+++ ChatAppSignalR/wwwroot/js/site.js
@@ -1,1 +1,28 @@
-﻿
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+
+function start() {
+    try {
+        connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+connection.on("ReceiveMessage", function (message) {
+    let messageRow = document.createElement("tr");
+    messageRow.appendChild(document.createElement("td"));
+    messageRow.appendChild(document.createElement("td"));
+
+    messageRow.firstChild.textContent = nameInput.value;
+    messageRow.lastChild.textContent = message;
+
+    messageList.appendChild(messageRow);
+})
+
+start();
+
+//connection.invoke("SendToAllUsers", "Welcome all!");
