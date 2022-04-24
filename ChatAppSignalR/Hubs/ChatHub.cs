116c7a63da1c63e5ceb875ac4481cd393a820887
csuchorski <cezary.suchorski@wp.pl>
2022-04-24 22:20:15 +0200

    Modified methods to find, join and leave chatrooms

--- ChatAppSignalR/Hubs/ChatHub.cs
+++ ChatAppSignalR/Hubs/ChatHub.cs
@@ -4,18 +4,21 @@ namespace ChatAppSignalR.Hubs
 {
     public class ChatHub : Hub
     {
-        public Dictionary<string, bool> groupNames = new Dictionary<string, bool>();
+        public Dictionary<string, byte> groupNames = new Dictionary<string, byte>();
         public async Task JoinRoom(string roomName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-            await Clients.Group(roomName).SendAsync("ReceiveMessage",$"{Context.ConnectionId} joined the room.");
+            groupNames[roomName]++;
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} joined the room.");
         }
         public async Task LeaveRoom(string roomName)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+            groupNames[roomName]--;
+            if (groupNames[roomName] == 0) groupNames.Remove(roomName);
             await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} left the room.");
         }
-        
+
         public async Task SendToAllUsers(string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
@@ -23,12 +26,15 @@ namespace ChatAppSignalR.Hubs
 
         public string FindRoom()
         {
+            if (!groupNames.ContainsValue(1)) return "roomNull";
+
             foreach(var room in groupNames)
             {
-                if (room.Value) return room.Key;
+                if (room.Value == 1) return room.Key;
             }
-            return "roomNull";        
+
+            return "roomNull";
         }
 
-    }
+}
 }
